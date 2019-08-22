ab2a2c91bfbbfa0e6d12a7a4c457f81fa031e7ac
Мохнаткин Алексей <a.mokhnatkin@a-i.kz>
2019-08-22 12:22:09 +0600

    CORS to backend

--- server-flask/app/__init__.py
+++ server-flask/app/__init__.py
@@ -13,7 +13,8 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     db.init_app(app)
-    CORS(app)
+    #CORS(app)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
     with app.app_context():
         from app.main import bp as main_bp
         app.register_blueprint(main_bp)
