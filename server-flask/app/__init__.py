44f321a1a609be292b9b3493b2b6dfa1a1f409a4
Alex <mokhnatkin@gmail.com>
2022-04-26 14:42:37 +0600

    backend logging added

--- server-flask/app/__init__.py
+++ server-flask/app/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask, request, current_app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 
 db = SQLAlchemy()
@@ -20,7 +21,22 @@ def create_app(config_class=Config):
         from app.errors import bp as errors_bp
         app.register_blueprint(errors_bp)        
         from app.api import bp as api_bp
-        app.register_blueprint(api_bp,url_prefix='/api')       
+        app.register_blueprint(api_bp,url_prefix='/api')
+
+        if not app.debug and not app.testing:
+            if not os.path.exists(app.config['LOGS_FOLDER']):
+                os.mkdir(app.config['LOGS_FOLDER'])
+            file_handler = RotatingFileHandler(os.path.join(app.config['LOGS_FOLDER'],'app_log.log'),
+                                                maxBytes=20000, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                    '%(asctime)s %(levelname)s: %(message)s '
+                    '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+            app.logger.setLevel(logging.INFO)
+            app.logger.info('Kaizen Soft startup')
+
     return app
 
 
--- server-flask/config.py
+++ server-flask/config.py
@@ -8,3 +8,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    LOGS_FOLDER = os.environ.get('LOGS_FOLDER')
